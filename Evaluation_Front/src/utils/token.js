16c18a3286888759adcac4af035dfcf4607e03eb
LiuYuYing03 <3190105132@zju.edu.cn>
2022-06-15 20:47:16 +0800

    修复登录跳转

--- Evaluation_Front/src/utils/token.js
+++ Evaluation_Front/src/utils/token.js
@@ -34,10 +34,11 @@ const verifyToken = () => {
     //
     const token = getToken()
     if( token ){
-            var url="http://124.220.171.17:3000/api/oauth/verify"
-            url = url + '?token=' + token
+            const url="http://124.220.171.17:3000/api/oauth/verify"
             console.log(url)
-            axios.get(url
+            axios.get(url,{
+                headers : {'token':token}
+                }
             ).then( function (res){
                 console.log(res)
                 if( res.data.code != 0) {
@@ -54,61 +55,14 @@ const verifyToken = () => {
             })
     }
     if( goLogin == true ){
+        console.log("Hello World")
+        console.log("[REDIRECT] : GO LOGIN")
         const oldLocation = window.location
         var url = 'http://124.220.171.17:3000/login?redir='
         const nowUrl = oldLocation.toString().slice(7)
         url = url + encodeURIComponent(nowUrl)
         window.location.href = url
     }
-    // const qs =require('qs')
-    // console.log( window.location.href )
-    // const searchParams = qs.parse( window.location.href.split('?')[1] )
-    // console.log( searchParams )
-    // if( searchParams.hasOwnProperty('token') ){
-    //     setToken( searchParams.token )
-    //     window.localStorage.setItem('userName', searchParams.userName )
-    //     window.localStorage.setItem('userId', searchParams.userId)
-    // }else{
-    //     const oldLocation = window.location
-    //     var url = 'http://124.220.171.17:3000/login?redir='
-    //     const nowUrl = oldLocation.toString().slice(7)
-    //     url = url + encodeURIComponent(nowUrl)
-    //     //url = url + nowUrl
-    //     window.location.href = url
-    // }
-    // if (token != undefined) {
-    //     const url="http://124.220.171.17:3000/api/oauth/verify"
-    //     let formData=new FormData()
-    //     formData.append("token",token)
-    //     axios.get(url, {
-    //         params: {
-    //             token: token
-    //         }
-    //     }).then( function (res){
-    //         if( res.data.code != 0) {
-    //             console.log("The token is false")
-    //             const oldLocation = window.location
-    //             var url = 'http://124.220.171.17:3000/login?redir='
-    //             const nowUrl = oldLocation.toString().slice(7)
-    //             url = url + encodeURIComponent(nowUrl)
-    //             //url = url + nowUrl
-    //             window.location.href = url
-    //         }else{
-    //             window.localStorage.setItem('userName', res.data.data.userName)
-    //             setToken(token)
-    //             console.log("The token is true")
-    //         }
-    //     }).catch(function (error){
-    //         console.log(error)
-    //     })
-    // }else{
-    //     const oldLocation = window.location
-    //     var url = 'http://124.220.171.17:3000/login?redir='
-    //     const nowUrl = oldLocation.toString().slice(7)
-    //     url = url + encodeURIComponent(nowUrl)
-    //     //url = url + nowUrl
-    //     window.location.href = url
-    // }
 }
 
 export {
